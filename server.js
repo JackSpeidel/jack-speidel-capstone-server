d766355c110de2a24d96b8923be15d28d7c98256
jack <speideljackson@protonmail.com>
2023-07-30 18:24:32 -0400

    rerouting articles

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const userRoute = require('./routes/userRoute');
+// const articlesRoute = require('.routes/articlesRoute');
 const port = process.env.PORT || 5050;
 
 // Middleware
@@ -12,7 +13,7 @@ app.use(cors());
 
 // Routing
 app.use('/api/users', userRoute);
-
+// app.use('/api/articles', articlesRoute);
 
 // Listen - Keep at Bottom
 app.listen(port, () => {
